0153ef89264f1ffce32a6e94be9a5dc11426f2e7
Nester Zerpa <nzerp263@mtroyal.ca>
2024-10-17 23:17:07 -0600

    It finishes driver page.

--- controller/Drivers.php
+++ controller/Drivers.php
@@ -15,7 +15,7 @@
       try {
 
         if (isset($driverRef)) {
-          $sql = "SELECT forename, surname, dob, nationality, url FROM drivers WHERE driverRef = '" . $driverRef . "' LIMIT 1";
+          $sql = "SELECT forename, surname, dob, nationality, url, code FROM drivers WHERE driverRef = '" . $driverRef . "' LIMIT 1";
         }
         
         $result = $this->pdo->query($sql);
@@ -28,13 +28,13 @@
 
     public function getDriverRaces($surname) {
       try {
-        $sql = 
-         "SELECT RA.name, RE.position, RE.points 
-          FROM drivers DR 
+        $sql = "SELECT RA.date as date, RA.name as race_name, CO.name as co_name, CI.name as ci_name, CI.country, RE.position, RE.number, RE.grid, RE.points, RE.time, RE.fastestLapTime, RE.fastestLapSpeed FROM drivers DR 
           INNER JOIN results RE ON DR.driverId = RE.driverId 
           INNER JOIN races RA ON RE.raceId = RA.raceId 
+          INNER JOIN constructors CO ON RE.constructorId = CO.constructorId
+          INNER JOIN circuits CI ON RA.circuitId = CI.circuitId
           WHERE DR.driverRef = '" . $surname . "' AND RA.year = 2022
-          ORDER BY RA.name;
+          ORDER BY RA.round;
         ";
         $result = $this->pdo->query($sql);
         return json_encode($result->fetchAll(PDO::FETCH_ASSOC)); 
@@ -43,6 +43,44 @@
         die( $e->getMessage() );
      } 
     }
+
+    function getDriverDetails() {
+      $driver = new Drivers();
+      $driver = json_decode($driver->drivers($_GET['driverRef']), true);
+      foreach ($driver as $row) {
+        $driverDetails = array(
+          "flag" => $this->getFlag($row['nationality']),
+          "forename" => $row['forename'],
+          "surname" => $row['surname'],
+          "dob" => $row['dob'],
+          "age" => $this->getAge($row['dob']),
+          "nationality" => $row['nationality'],
+          "url" => $row['url'],
+          "code" => $row['code']
+        );
+      }
+
+      return $driverDetails;
+    }
+
+    function getAge($dob) {
+      if(isset($dob)) {
+        $dob = new DateTime($dob);
+        $current = new DateTime(date("Y-m-d H:i:s"));
+        $age = $current->diff($dob)->y;
+      } else {
+        $age = "Unknown";
+      }
+
+      return $age;
+    }
+
+    function getFlag($nationality) {
+      $countries = require 'countriesWithCode.php'; // Use require instead of require_once
+      
+      return $countries[$nationality] ?? null; // Return null if nationality not found
+    }
+
   }
 
 ?>
--- drivers.php
+++ drivers.php
@@ -3,46 +3,10 @@
   require_once('header.php');
   require_once('controller/Drivers.php');
  
- 
- function getDriverDetails() {
-    $driver = new Drivers();
-    $driver = json_decode($driver->drivers($_GET['driverRef']), true);
-    foreach ($driver as $row) {
-      $driverDetails = array(
-        "flag" => getFlag($row['nationality']),
-        "forename" => $row['forename'],
-        "surname" => $row['surname'],
-        "dob" => $row['dob'],
-        "age" => getAge($row['dob']),
-        "nationality" => $row['nationality'],
-        "url" => $row['url']
-      );
-    }
-
-    return $driverDetails; // Return the flags array if needed
- }
-
- function getAge($dob) {
-    if(isset($dob)) {
-      $dob = new DateTime($dob);
-      $current = new DateTime(date("Y-m-d H:i:s"));
-      $age = $current->diff($dob)->y;
-    } else {
-      $age = "Unknown";
-    }
-
-    return $age;
- }
-
- function getFlag($nationality) {
-  $countries = require 'countriesWithCode.php'; // Use require instead of require_once
-  
-  return $countries[$nationality] ?? null; // Return null if nationality not found
-}
+  $driver = new Drivers();
 
   function getDriverRaces() {
     $driver = new Drivers();
-
 
     $driverRaces = json_decode($driver->getDriverRaces($_GET['driverRef']), true);
 
@@ -50,24 +14,34 @@
       echo("<table class='table table-dark table-hover'>");
       echo("<thead>
             <tr>
+              <th>Race</th>
+              <th>Constructor</th>
               <th>Circuit</th>
-              <th>Position</th>
+              <th>Country</th>
+              <th>Final Position</th>
+              <th>Grid</th>
               <th>Points</th>
+              <th>Fastest Lap Time</th>
+              <th>Fastest Lap Speed</th>
             </tr>
           </thead>");
       foreach ($driverRaces as $race) {
         echo("<tr>");
-          echo("<td> " . $race['name'] . " </td>");
+          echo("<td> " . $race['race_name'] . " </td>");
+          echo("<td> " . $race['co_name'] . " </td>");
+          echo("<td> " . $race['ci_name'] . " </td>");
+          echo("<td> " . $race['country'] . " </td>");
           echo("<td> " . $race['position'] . " </td>");
+          echo("<td> " . $race['grid'] . " </td>");
           echo("<td> " . $race['points'] . " </td>");
+          echo("<td> " . $race['fastestLapTime'] . " </td>");
+          echo("<td> " . $race['fastestLapSpeed'] . " </td>");
         echo("</tr>");
       }
       echo("</table>");
     } else {
       echo "No data available";
     }
-    
-
   }
 
 ?>
@@ -77,15 +51,27 @@
       <div class="container-fluid">
           <div class="row g-0">
               <div class="col-4">
-                <h3> Drivers </h3>
-                <?php 
-                  $driver = getDriverDetails();
-                  echo("<br/>Nationality: <img height='100' width='100' src='public/driver1.png'></img>");
-                  echo("Forename: " . $driver['forename']);
-                  echo("<br/>Surname: ". $driver['surname']);
-                  echo("<br/>Age: " . $driver['age']);
-                  echo("<br/>Nationality: <img height='100' width='100' src='https://raw.githubusercontent.com/lipis/flag-icons/02b8adceb338125c61f7a1d64d6e5bd9826ae427/flags/1x1/".$driver['flag'].".svg'></img>");
-                  echo("<br/>URL: " . $driver['url']);
+              <?php
+                  $driver = $driver->getDriverDetails();
+                  echo 
+                  "<div class='card' style='max-width:80%'>
+                    <img src='public/driver1.png' class='card-img-top' alt='...'>
+                    <div class='card-body'>
+                      <h5 class='card-title'>" . $driver['forename'] . " " . $driver['surname'] . "</h5>
+                    </div>
+                    <ul class='list-group list-group-flush'>
+                      <li class='list-group-item'>" . $driver['age'] . " Years Old </li>
+                      <li class='list-group-item'>" . $driver['code'] . " </li>
+                      <li class='list-group-item'>
+                          <img class='mx-auto d-block img-fluid' style='border: 1px solid black;' height='100' width='100' src='https://raw.githubusercontent.com/lipis/flag-icons/02b8adceb338125c61f7a1d64d6e5bd9826ae427/flags/1x1/" . $driver['flag'] . ".svg' />
+                      </li>
+
+                    </ul>
+                    <div class='card-body'>
+                      <a href='" . $driver['url'] . "' class='card-link'>Click ME For More</a>
+                    </div>
+                  </div>";
+                
                 ?>
               </div>
               <div class="col-8">
